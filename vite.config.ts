0ddac2362291f7293bba4c15e233886e987561af
t0m0m0 <aka.tomo06@gmail.com>
2026-02-11 11:05:14 +0900

    fix:loadenv

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import devServer from '@hono/vite-dev-server'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    devServer({
-      entry: 'src/server/index.ts',
-      exclude: [/^\/(?!api\/).*/],
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  Object.assign(process.env, env)
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      devServer({
+        entry: 'src/server/index.ts',
+        exclude: [/^\/(?!api\/).*/],
+      }),
+    ],
+  }
 })
